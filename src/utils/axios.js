f051ba7e55bf9bef411ade056bc96fd103a1d9c1
Aman Kumar <amanverma2457@gmail.com>
2024-06-18 12:52:51 +0530

    add env for api url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://team-management-api-service.vercel.app',
+    baseURL: process.env.REACT_APP_API_URL,
     timeout: 5000,
 });
 
